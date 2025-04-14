89d26f066ab6c023d0f98d0f0e4be81bd1f055ad
Teguh Suryo Santoso <teguh.s.santoso@gdplabs.id>
2025-04-14 11:32:29 +0700

    Add gl-chat-widget component in standalone config

--- vite.standalone.ts
+++ vite.standalone.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     lib: {
-      entry: ["src/components/webcam.ts"],
+      entry: ["src/components/gl-chat-widget.ts", "src/components/webcam.ts"],
       formats: ["es"],
     },
     emptyOutDir: false,
