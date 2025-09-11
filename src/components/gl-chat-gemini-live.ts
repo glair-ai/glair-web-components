bc2fdd6c9406e6c8d263c0b3161b3f1664b5cc06
addffa-gl <78341292+addffa-gl@users.noreply.github.com>
2025-09-11 10:44:25 +0700

    enhancement: remove reset session

--- src/components/gl-chat-gemini-live.ts
+++ src/components/gl-chat-gemini-live.ts
@@ -394,12 +394,6 @@ export class GLChatGeminiLive extends LitElement {
     this.updateStatus("Recording stopped. Click Start to begin again.");
   }
 
-  private reset() {
-    this.session?.close();
-    this.initSession();
-    this.updateStatus("Session cleared.");
-  }
-
   protected firstUpdated() {
     this.initClient();
   }
@@ -415,23 +409,6 @@ export class GLChatGeminiLive extends LitElement {
         <div style="display: none">${this.status}</div>
         <div id="captions">${this.modelTranscript}</div>
         <div class="controls">
-          <button
-            id="resetButton"
-            @click=${this.reset}
-            ?disabled=${this.isRecording}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="40px"
-              viewBox="0 -960 960 960"
-              width="40px"
-              fill="#ffffff"
-            >
-              <path
-                d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"
-              />
-            </svg>
-          </button>
           <button
             id="startButton"
             @click=${this.startRecording}
